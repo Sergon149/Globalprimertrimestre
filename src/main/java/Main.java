db14771ff337e3ec4ce0efd9612670f97dedf865
Sergon149 <sergon149@alu.centroafuera.es>
2022-01-24 19:59:08 +0100

    recu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,47 +1,81 @@
 import java.util.Random;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantLock;
 
-
 public class Main {
-    static AtomicInteger listos = new AtomicInteger(0);
-    static AtomicInteger puestos = new AtomicInteger(2);
-    static Semaphore s22 = new Semaphore(0,true);
-
     public static void main(String[] args) {
-        Semaphore s1 = new Semaphore(10,true);
-        Semaphore s2 = Main.s22;
-        ReentrantLock s3 = new ReentrantLock();
 
-        for (int i=1;i<=20;i++){
-            Jugador j = new Jugador(i,s1,s2,s3);
+        for (int i = 0; i < 20; i++) {
+            Jugador j = new Jugador();
+            j.setName("Hilo " + i);
             j.start();
         }
     }
-    public synchronized static void puestoss(Jugador jugador) {
-        int puesto=Main.puestos.get();
-
-        if (puesto<=5){
-            System.out.println("El jugador "+jugador.id+ " ha quedado en  la posicion "+ puesto);
-        } else {
-            System.out.println("El jugador "+jugador.id+ " no ha llegado a tiempo a la prueba 2 y ha sido descalificado");
-        }
-
-        Main.puestos.set(puesto+1);
-
-    }
-
-    public synchronized static void listoss() {
-        Main.listos.set(Main.listos.get()+1);
-        if (Main.listos.get() == 10){
-            Main.s22.release(10);
-        }
-
-    }
-
 }
 
 
 
+class Jugador extends Thread{
+    private boolean esPrimero;
 
+    @Override
+    public void run() {
+        Random r = new Random();
+        int dormir= r.nextInt((4)+1)*1000;
+        int posib= r.nextInt(11);
+        try {
+            Thread.sleep(dormir);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (posib == 1){
+            System.out.println("El jugador "+getName()+" ha sido descalificado de la prueba 1");
+        }else{
+            System.out.println("El jugador "+getName()+" ha superado la prueba 1");
+            CampoDeBatalla.addParticipante(this);
+        }
+    }
+    public void setEsPrimero(boolean esPrimero) {
+        this.esPrimero = esPrimero;
+    }
+}
+
+
+
+class CampoDeBatalla{
+
+    private static final Semaphore semaphore = new Semaphore(10, false);
+    static ReentrantLock lock = new ReentrantLock();
+    private static int contpr=0;
+
+    public static void addParticipante(Jugador jugador) {
+        try {
+            lock.lock();
+            semaphore.acquire(10);
+            lock.unlock();
+            System.out.println(jugador.getName() + " ha completado a tiempo la puerba 1");
+            semaphore.release(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Random r = new Random();
+        int posib = r.nextInt(11);
+        if (posib == 1) {
+            System.out.println("El jugador " + jugador.getName() + " ha sido descalificado de la prueba 2");
+        } else {
+            System.out.println("El jugador " + jugador.getName() + " ha superado la prueba 2");
+            contpr++;
+            if (contpr == 1) {
+                jugador.setEsPrimero(true);
+                System.out.println("El jugador " + jugador.getName() + " ha ganado el Battle Royale de Warzone 2.0");
+            }else if (contpr<6 & contpr>1){
+                System.out.println("El jugador "+jugador.getName()+ " ha quedado en la posicion "+contpr+" del Battle Royale de Warzone 2.0");
+            }else{
+                System.out.println("El jugador "+jugador.getName()+" no ha llegado a tiempo a la prueba 2 y ha sido descalificado");
+            }
+        }
+
+    }
+}
